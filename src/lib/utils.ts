a960b1be21ef3d1bd36cece7ff248759998ee047
Lance Liang <liangxianyi2011@gmail.com>
2023-07-18 20:57:04 -0400

    Add tag filter

---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+export function slugify(text: string) {
+  if (!text) throw new Error("no input");
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+}
